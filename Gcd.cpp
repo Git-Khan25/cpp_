bbb5972427fe1c5957186dd5f59e69ddc0d8b0df
Mohammed Faroz Khan <muhammadfarozkhan786@gmail.com>
2026-01-24 23:25:25 +0530

    Add files via upload

---
+++ Gcd.cpp
@@ -0,0 +1,33 @@
+//gcd of 2 num in cpp using class
+#include <iostream>
+using namespace std;    
+class GCD {
+private:
+    int num1, num2;
+
+    int GCDcalc(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+public:
+
+    void input() {
+        cout << "Enter two positive integers: ";
+        cin >> num1 >> num2;
+    }
+
+    void displayGCD() {
+        int gcd = GCDcalc(num1, num2);
+        cout << "GCD of " << num1 << " and " << num2 << " is: " << gcd << endl;
+    }
+};  
+int main() {
+    GCD gcdCalculator;
+    gcdCalculator.input();
+    gcdCalculator.displayGCD();
+    return 0;
+}
