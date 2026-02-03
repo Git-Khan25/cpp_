2793d190faf78450e8b4e2c1cfe13a3debe58931
Mohammed Faroz Khan <muhammadfarozkhan786@gmail.com>
2026-02-03 15:02:55 +0530

    Add files via upload

---
+++ defaultconc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+class student{
+    int roll;
+    public:
+    student(){
+    
+        roll=0;
+    }
+     
+  void display(){
+      cout<<"Roll: "<<roll<<endl;
+  }
+  
+};
+int main(){
+    student s1;
+    s1.display();
+
+    return 0;
+}
